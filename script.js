4442a26a9cda2ef67e5c3134454c41a103affc67
Владимир Кудряшов <thekudryashow@MacBook-Air-Vladimir.local>
2021-02-02 14:39:37 +0200

    сделал домашнее задание 1 по JS

---
+++ script.js
@@ -0,0 +1,11 @@
+let name = prompt("Как вас зовут");
+let age = prompt("Сколько вам лет");
+let str = `Привет! ${name}.`;
+
+if (age <= 16) {
+    alert (`${str} Вы подросток`);
+} else if (16 < age && age <= 40) {
+    alert (`${str} Вы молодой человек`);
+} else {
+    alert (`${str} Вы старичок`);
+}
