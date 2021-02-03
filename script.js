0b29d6cc2c096e66ca6e6272f901f6aa55949576
Vladimir <artcandidate@gmail.com>
2021-02-03 13:03:10 +0200

    исправил домашнее задание по JS

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ let name = prompt("Как вас зовут");
 let age = prompt("Сколько вам лет");
 let str = `Привет! ${name}.`;
 
-if (age <= 16) {
+if (age <= 0) {
+    alert (`${str} Вы ввели отрицательный возраст`);
+} else if (age <= 16) {
     alert (`${str} Вы подросток`);
 } else if (16 < age && age <= 40) {
     alert (`${str} Вы молодой человек`);
